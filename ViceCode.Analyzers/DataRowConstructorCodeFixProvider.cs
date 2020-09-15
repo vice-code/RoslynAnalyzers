9df40863eab316701a2882717d905b2fa576587d
LIFEfreedom <lifefreedom11@gmail.com>
2020-09-15 17:47:43 +0300

    fix copying comment from first property

--- ViceCode.Analyzers/DataRowConstructorCodeFixProvider.cs
+++ ViceCode.Analyzers/DataRowConstructorCodeFixProvider.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
 using System.Data;
@@ -6,177 +12,173 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeActions;
-using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-
 namespace ViceCode.Analyzers
 {
-    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(DataRowConstructorCodeFixProvider)), Shared]
-    public class DataRowConstructorCodeFixProvider : CodeFixProvider
-    {
-        // <SnippetCodeFixTitle>
-        private const string titleCreate = "Generate constructor(DataRow row)";
-        private const string titleUpdate = "Update constructor(DataRow row)";
-        // </SnippetCodeFixTitle>
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(DataRowConstructorCodeFixProvider)), Shared]
+	public class DataRowConstructorCodeFixProvider : CodeFixProvider
+	{
+		// <SnippetCodeFixTitle>
+		private const string titleCreate = "Generate constructor(DataRow row)";
+		private const string titleUpdate = "Update constructor(DataRow row)";
+		// </SnippetCodeFixTitle>
 
-        public sealed override ImmutableArray<string> FixableDiagnosticIds
-        {
-            get { return ImmutableArray.Create(DataRowConstructorAnalyzer.CreateDataRowConstructorDiagnosticId, DataRowConstructorAnalyzer.UpdateDataRowConstructorDiagnosticId); }
-        }
+		public sealed override ImmutableArray<string> FixableDiagnosticIds
+		{
+			get { return ImmutableArray.Create(DataRowConstructorAnalyzer.CreateDataRowConstructorDiagnosticId, DataRowConstructorAnalyzer.UpdateDataRowConstructorDiagnosticId); }
+		}
 
-        public sealed override FixAllProvider GetFixAllProvider()
-        {
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
-            return WellKnownFixAllProviders.BatchFixer;
-        }
+		public sealed override FixAllProvider GetFixAllProvider()
+		{
+			// See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+			return WellKnownFixAllProviders.BatchFixer;
+		}
 
-        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
-        {
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken);
+		public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken);
 
-            var diagnostic = context.Diagnostics.First();
-            var diagnosticSpan = diagnostic.Location.SourceSpan;
+			var diagnostic = context.Diagnostics.First();
+			var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-            // Find the type declaration identified by the diagnostic.
-            var classDeclation = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ClassDeclarationSyntax>().First();
+			// Find the type declaration identified by the diagnostic.
+			var classDeclation = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ClassDeclarationSyntax>().First();
 
-            if (diagnostic.Id == DataRowConstructorAnalyzer.CreateDataRowConstructorDiagnosticId)
-            {
-                // Register a code action that will invoke the fix.
-                context.RegisterCodeFix(
-                    CodeAction.Create(
-                        title: titleCreate,
-                        createChangedDocument: ct => CreateDataRowConstructor(context.Document, classDeclation, ct),
-                        equivalenceKey: titleCreate),
-                    diagnostic);
-                return;
-            }
-
-            var constructorDeclation = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ConstructorDeclarationSyntax>().First();
-
-            // Register a code action that will invoke the fix.
-            context.RegisterCodeFix(
-                CodeAction.Create(
-                    title: titleUpdate,
-                    createChangedDocument: ct => UpdateDataRowConstructor(context.Document, classDeclation, constructorDeclation, ct),
-                    equivalenceKey: titleUpdate),
-                diagnostic);
-        }
-
-        private async Task<Document> UpdateDataRowConstructor(Document document, ClassDeclarationSyntax classDeclaration, ConstructorDeclarationSyntax constructorDeclaration, CancellationToken ct)
-        {
-            var unsetProperties = Helper.GetClassUnsetProperties(classDeclaration, constructorDeclaration);
-
-            SyntaxTriviaList leadingTrivia;
-            SyntaxList<StatementSyntax> statements;
-
-            if (constructorDeclaration.Body is null)
-            {
-                leadingTrivia = constructorDeclaration.GetLeadingTrivia();
-            }
-            else
-            {
-                statements = constructorDeclaration.Body.Statements;
-                leadingTrivia = statements.Any() ? statements.First().GetLeadingTrivia() : constructorDeclaration.GetLeadingTrivia();
+			if (diagnostic.Id == DataRowConstructorAnalyzer.CreateDataRowConstructorDiagnosticId)
+			{
+				// Register a code action that will invoke the fix.
+				context.RegisterCodeFix(
+					CodeAction.Create(
+						title: titleCreate,
+						createChangedDocument: ct => CreateDataRowConstructor(context.Document, classDeclation, ct),
+						equivalenceKey: titleCreate),
+					diagnostic);
+				return;
 			}
 
-            foreach (var property in unsetProperties)
-            {
-                var assigment = CreatePropertyAssigmentExpression(constructorDeclaration.ParameterList.Parameters[0], property);
+			var constructorDeclation = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ConstructorDeclarationSyntax>().First();
 
-                statements = statements.Add(SyntaxFactory.ExpressionStatement(assigment).WithLeadingTrivia(leadingTrivia));
-            }
+			// Register a code action that will invoke the fix.
+			context.RegisterCodeFix(
+				CodeAction.Create(
+					title: titleUpdate,
+					createChangedDocument: ct => UpdateDataRowConstructor(context.Document, classDeclation, constructorDeclation, ct),
+					equivalenceKey: titleUpdate),
+				diagnostic);
+		}
 
-            // Копируем параметры конструктора, заменяя только Body.
-            var updatedConstructor = SyntaxFactory.ConstructorDeclaration(constructorDeclaration.AttributeLists, constructorDeclaration.Modifiers, constructorDeclaration.Identifier, constructorDeclaration.ParameterList, constructorDeclaration.Initializer, SyntaxFactory.Block(statements));
+		private async Task<Document> UpdateDataRowConstructor(Document document, ClassDeclarationSyntax classDeclaration, ConstructorDeclarationSyntax constructorDeclaration, CancellationToken ct)
+		{
+			var unsetProperties = Helper.GetClassUnsetProperties(classDeclaration, constructorDeclaration);
 
-            // Replace the old constructor declaration with the new local declaration.
-            var oldRoot = await document.GetSyntaxRootAsync(ct);
-            var newRoot = oldRoot.ReplaceNode(constructorDeclaration, updatedConstructor);
+			SyntaxTriviaList leadingTrivia;
+			SyntaxList<StatementSyntax> statements;
 
-            // Return document with transformed tree.
-            return document.WithSyntaxRoot(newRoot);
-        }
+			if (constructorDeclaration.Body is null)
+			{
+				leadingTrivia = constructorDeclaration.GetLeadingTrivia();
+			}
+			else
+			{
+				statements = constructorDeclaration.Body.Statements;
+				leadingTrivia = statements.Any() ? statements.First().GetLeadingTrivia() : constructorDeclaration.GetLeadingTrivia();
+			}
 
-        private async Task<Document> CreateDataRowConstructor(Document document, ClassDeclarationSyntax classDeclaration, CancellationToken ct)
-        {
-            // attributes;
-            var attributes = new SyntaxList<AttributeListSyntax>();
+			foreach (var property in unsetProperties)
+			{
+				var assigment = CreatePropertyAssigmentExpression(constructorDeclaration.ParameterList.Parameters[0], property);
 
-            // Modifiers;
-            const string publicText = "public ";
-            var publicModifier = SyntaxFactory.Identifier(SyntaxTriviaList.Empty, SyntaxKind.PublicKeyword, publicText, publicText, SyntaxTriviaList.Empty);
-            var modifiers = new SyntaxTokenList(publicModifier);
+				statements = statements.Add(SyntaxFactory.ExpressionStatement(assigment).WithLeadingTrivia(leadingTrivia));
+			}
 
-            // Identifier;
-            var identifier = SyntaxFactory.Identifier(SyntaxTriviaList.Empty, SyntaxKind.PublicKeyword, classDeclaration.Identifier.Text, classDeclaration.Identifier.ValueText, SyntaxTriviaList.Empty).WithLeadingTrivia().WithTrailingTrivia();
+			// Копируем параметры конструктора, заменяя только Body.
+			var updatedConstructor = SyntaxFactory.ConstructorDeclaration(constructorDeclaration.AttributeLists, constructorDeclaration.Modifiers, constructorDeclaration.Identifier, constructorDeclaration.ParameterList, constructorDeclaration.Initializer, SyntaxFactory.Block(statements));
 
-            // ParameterList;
-            var parameterName = SyntaxFactory.Identifier("row").WithTrailingTrivia().WithLeadingTrivia();
-            var parameterType = SyntaxFactory.IdentifierName(typeof(DataRow).Name);
-            var parameter = SyntaxFactory.Parameter(new SyntaxList<AttributeListSyntax>(), new SyntaxTokenList(), parameterType, parameterName, null);
-            var parameters = SyntaxFactory.ParameterList(SyntaxFactory.SeparatedList(new[] { parameter }));
+			// Replace the old constructor declaration with the new local declaration.
+			var oldRoot = await document.GetSyntaxRootAsync(ct);
+			var newRoot = oldRoot.ReplaceNode(constructorDeclaration, updatedConstructor);
 
-            // Body block
-            var properties = classDeclaration.Members.OfType<PropertyDeclarationSyntax>().ToList();
-            var statements = new List<StatementSyntax>(properties.Count);
+			// Return document with transformed tree.
+			return document.WithSyntaxRoot(newRoot);
+		}
 
-            foreach (var property in properties)
-            {
-                statements.Add(SyntaxFactory.ExpressionStatement(CreatePropertyAssigmentExpression(parameter, property)));
-            }
+		private async Task<Document> CreateDataRowConstructor(Document document, ClassDeclarationSyntax classDeclaration, CancellationToken ct)
+		{
+			// attributes;
+			var attributes = new SyntaxList<AttributeListSyntax>();
 
-            var block = SyntaxFactory.Block(statements);
+			// Modifiers;
+			const string publicText = "public ";
+			var publicModifier = SyntaxFactory.Identifier(SyntaxTriviaList.Empty, SyntaxKind.PublicKeyword, publicText, publicText, SyntaxTriviaList.Empty);
+			var modifiers = new SyntaxTokenList(publicModifier);
 
-            var constructor = SyntaxFactory.ConstructorDeclaration(attributes, modifiers, identifier, parameters, null, block);
+			// Identifier;
+			var identifier = SyntaxFactory.Identifier(SyntaxTriviaList.Empty, SyntaxKind.PublicKeyword, classDeclaration.Identifier.Text, classDeclaration.Identifier.ValueText, SyntaxTriviaList.Empty)/*.WithLeadingTrivia().WithTrailingTrivia()*/;
 
-            var newClassDexlaration = classDeclaration.InsertNodesBefore(classDeclaration.Members.FirstOrDefault(), new[] { constructor.WithLeadingTrivia(properties.First().GetLeadingTrivia()) });
+			// ParameterList;
+			var parameterName = SyntaxFactory.Identifier("row").WithTrailingTrivia().WithLeadingTrivia();
+			var parameterType = SyntaxFactory.IdentifierName(typeof(DataRow).Name);
+			var parameter = SyntaxFactory.Parameter(new SyntaxList<AttributeListSyntax>(), new SyntaxTokenList(), parameterType, parameterName, null);
+			var parameters = SyntaxFactory.ParameterList(SyntaxFactory.SeparatedList(new[] { parameter }));
 
-            var root = await document.GetSyntaxRootAsync(ct);
+			// Body block
+			var properties = classDeclaration.Members.OfType<PropertyDeclarationSyntax>().ToList();
+			var statements = new List<StatementSyntax>(properties.Count);
 
-            CompilationUnitSyntax newRoot = (CompilationUnitSyntax)root.ReplaceNode(classDeclaration, newClassDexlaration);
+			foreach (var property in properties)
+			{
+				statements.Add(SyntaxFactory.ExpressionStatement(CreatePropertyAssigmentExpression(parameter, SyntaxFactory.PropertyDeclaration(property.Type, property.Identifier))));
+			}
 
-            bool usingFinded = false;
-            foreach(var usingDirective in newRoot.Usings)
-            {
-                if(usingDirective.Name.ToString().Equals("System.Data"))
-                {
+			var block = SyntaxFactory.Block(statements);
+
+			var constructor = SyntaxFactory.ConstructorDeclaration(attributes, modifiers, identifier, parameters, null, block);
+
+			var fpt = properties.First().GetLeadingTrivia().Where(t => t.IsKind(SyntaxKind.WhitespaceTrivia)).First();
+
+			var newClassDexlaration = classDeclaration.InsertNodesBefore(classDeclaration.Members.FirstOrDefault(), new[] { constructor.WithLeadingTrivia(fpt) });
+
+			var root = await document.GetSyntaxRootAsync(ct);
+
+			CompilationUnitSyntax newRoot = (CompilationUnitSyntax)root.ReplaceNode(classDeclaration, newClassDexlaration);
+
+			bool usingFinded = false;
+			foreach (var usingDirective in newRoot.Usings)
+			{
+				if (usingDirective.Name.ToString().Equals("System.Data"))
+				{
 					usingFinded = true;
 					break;
 				}
 			}
 
-            if(!usingFinded)
-            {
-                newRoot = newRoot.AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName("System.Data")));
-            }
+			if (!usingFinded)
+			{
+				newRoot = newRoot.AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName("System.Data")));
+			}
 
-            return document.WithSyntaxRoot(newRoot);
-        }
+			return document.WithSyntaxRoot(newRoot);
+		}
 
-        private static AssignmentExpressionSyntax CreatePropertyAssigmentExpression(ParameterSyntax parameter, PropertyDeclarationSyntax property)
-        {
-            var left = SyntaxFactory.IdentifierName(property.Identifier);
+		private static AssignmentExpressionSyntax CreatePropertyAssigmentExpression(ParameterSyntax parameter, PropertyDeclarationSyntax property)
+		{
+			var left = SyntaxFactory.IdentifierName(property.Identifier);
 
-            // GenericName
-            var typeSeparatedList = SyntaxFactory.SeparatedList(new[] { property.Type });                                                       // {property type}
-            var typeArgumentList = SyntaxFactory.TypeArgumentList(typeSeparatedList);                                                           // <{property type}>
-            var generic = SyntaxFactory.Identifier("Field");                                                                                    // Field
-            var genericName = SyntaxFactory.GenericName(generic, typeArgumentList);                                                             // Field<{Property type}>
+			// GenericName
+			var typeSeparatedList = SyntaxFactory.SeparatedList(new[] { property.Type });                                                       // {property type}
+			var typeArgumentList = SyntaxFactory.TypeArgumentList(typeSeparatedList);                                                           // <{property type}>
+			var generic = SyntaxFactory.Identifier("Field");                                                                                    // Field
+			var genericName = SyntaxFactory.GenericName(generic, typeArgumentList);                                                             // Field<{Property type}>
 
-            var rowParam = SyntaxFactory.IdentifierName(parameter.Identifier);                                                                  // row
-            var memberAccess = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, rowParam, genericName);            // add Field<{Property type}> to row
-            var text = $"\"{property.Identifier.Text}\"";                                                                                       // ( "{Property name}")
-            var argName = SyntaxFactory.Token(SyntaxTriviaList.Empty, SyntaxKind.StringLiteralToken, text, text, SyntaxTriviaList.Empty);
-            var argument = SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, argName));
-            var separatedList = SyntaxFactory.SeparatedList(new[] { argument });
-            var argumentList = SyntaxFactory.ArgumentList(separatedList);
+			var rowParam = SyntaxFactory.IdentifierName(parameter.Identifier);                                                                  // row
+			var memberAccess = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, rowParam, genericName);            // add Field<{Property type}> to row
+			var text = $"\"{property.Identifier.Text}\"";                                                                                       // ( "{Property name}")
+			var argName = SyntaxFactory.Token(SyntaxTriviaList.Empty, SyntaxKind.StringLiteralToken, text, text, SyntaxTriviaList.Empty);
+			var argument = SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, argName));
+			var separatedList = SyntaxFactory.SeparatedList(new[] { argument });
+			var argumentList = SyntaxFactory.ArgumentList(separatedList);
 
-            var right = SyntaxFactory.InvocationExpression(memberAccess, argumentList);                                                         // row.Field<{property type}>("{property name}");
-            return SyntaxFactory.AssignmentExpression(SyntaxKind.SimpleAssignmentExpression, left, right);
-        }
-    }
+			var right = SyntaxFactory.InvocationExpression(memberAccess, argumentList);                                                         // row.Field<{property type}>("{property name}");
+			return SyntaxFactory.AssignmentExpression(SyntaxKind.SimpleAssignmentExpression, left, right);
+		}
+	}
 }
